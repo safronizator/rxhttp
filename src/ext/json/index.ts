8d961d1ffb767bc8ee9605d2e820e5fe0558b031
Aleksandr Safronov <me@safron.pro>
2020-03-02 21:09:52 +0300

    ext/json: middleware added: parseIfJson()

--- src/ext/json/index.ts
+++ src/ext/json/index.ts
@@ -3,22 +3,37 @@ import {ErrorHandlerFunc, HandlingError, Middleware, Renderer} from "../../handl
 import {RequestHeader, StatusCode} from "../../http";
 import {streamReadAllToString} from "../../helpers";
 import {BodyParsed, CustomResponseData} from "../common";
-import {Response} from "../../base";
+import {Context, Response} from "../../base";
+import {RequestInterface} from "../../interface";
 
 
-export const parseJson = (): Middleware<{}, BodyParsed> => source => source.pipe(mergeMap(async ctx => {
-    if (!ctx.request.headers.has(RequestHeader.ContentType, "application/json")) {
-        throw new HandlingError("Request body should contain JSON-encoded data", ctx, StatusCode.BadRequest);
-    }
+export const isJson = (request: RequestInterface): boolean => {
+    return request.headers.has(RequestHeader.ContentType, "application/json");
+};
+
+export const parseRequestBody = async (ctx: Context) => {
     try {
-        return ctx.withState({
-            parsedBody: JSON.parse(await streamReadAllToString(ctx.request.body))
-        });
+        return JSON.parse(await streamReadAllToString(ctx.request.body))
     } catch (e) {
         throw new HandlingError(e.message, ctx, StatusCode.BadRequest);
     }
-}));
+};
 
+export const parseJson = (): Middleware<{}, BodyParsed> => source => source.pipe(
+    mergeMap(async ctx => {
+        if (!isJson(ctx.request)) {
+            throw new HandlingError("Request body should contain JSON-encoded data", ctx, StatusCode.BadRequest);
+        }
+        return ctx.withState({ parsedBody: await parseRequestBody(ctx) });
+    }));
+
+export const parseIfJson = (): Middleware<{}, BodyParsed> => source => source.pipe(
+    mergeMap(async ctx => {
+        if (!isJson(ctx.request)) {
+            return ctx.withState({ parsedBody: null });
+        }
+        return ctx.withState({ parsedBody: await parseRequestBody(ctx) });
+    }));
 
 export const renderJson = (): Renderer<CustomResponseData> => source => source.pipe(
     map(ctx => Response.for(ctx).withJsonBody(ctx.state.responseData))
